3d97d69a2c4ad675e0961d299bbdb71e6520f59c
UiTae Kim <taejja09@gmail.com>
2024-11-20 13:22:53 +0900

    removed wrong server url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-const SERVER_URL = "http://127.0.0.1:8000";
+const SERVER_URL = '';
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
